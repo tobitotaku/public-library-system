e1a924edbeef89e1fd51a4827b9c5f7f2dcc19f5
tobitotaku <99360631+tobitotaku@users.noreply.github.com>
2022-04-04 22:35:50 +0200

    feat: added base ControllerView list members edit member

--- controllers.py
+++ controllers.py
@@ -4,69 +4,119 @@ from usermodels import *
 class ControllerView:
     def __init__(self, *args):
         self.user = False
-        self.render_login()
-        self.menu_option_list = {
-            1:"Add a member",
-        }
-        self.menu_action_list = {
-            1:"render_add_member",
-        }
-        self.render_menu_options()
+        self.menu_item = {}
+        self.menu_option_list = {}
+        self.menu_action_list = {}
+    
+    def set_menu_options_actions(self):
+        i = 0
+        for item in self.menu_item:
+            i += 1
+            # print(type(item))
+            self.menu_action_list[i] = item
+            self.menu_option_list[i] = self.menu_item[item]
 
-    def render_login(self):
-        inp_username = str(input('Username:'))
-        inp_password = str(input('Password:'))
-
-        _user = Person.findbyname(inp_username)
-        if (_user and inp_username == _user.username and inp_password == _user.password):
-            self.user = _user
-            print('Welcome,{username}!'.format(username = _user.username))
-        else:
-            print('Password or Username is incorrect')
-        return self.user
-
-    def render_menu_options(self):
-        print(
-            "Menu Options:"
-        )
+    def render_menu(self):
         inp_option = False
+        self.set_menu_options_actions()
         # _options.key should match _actions.key
         for item in self.menu_option_list:
             txt = "{number}. {label}".format(number = item, label = self.menu_option_list[item])
             print(txt)
         
-        try:
-            inp_option = int(input("Enter a menu option:"))
-        except:
-            print("invalid menu option. input should be integer")
-            
-        if (inp_option and inp_option in self.menu_action_list):
+        while True:
             try:
-                _method = getattr(self, self.menu_action_list[inp_option])
-                _method()
+                inp_option = int(input("Enter a menu option:"))
+                break
             except:
-                print("Error: action not found")
+                print("invalid menu option. input should be integer")
+                
+        if (inp_option and inp_option in self.menu_action_list):
+            # try:
+            _method = getattr(self, self.menu_action_list[inp_option])
+            _method()
+            # except:
+            #     print("Error: action not found")
         else:
             print("entered number is not a menu options")
 
         return inp_option
 
+class UsersCV(ControllerView):
+    def __init__(self, *args):
+        ControllerView.__init__(self, *args)
+        # self.user = False
+        self.render_login()
+        self.menu_item = {
+            "render_add_member":"Add a member",
+            "render_edit_member":"Edit a member",
+            "render_list_member":"List all members",
+            "render_delete_member":"Delete a member",
+        }
+        self.render_menu()
+
+    def render_login(self):
+        inp_username = str(input('Username:'))
+        inp_password = str(input('Password:'))
+
+        user = Person.findbyname(inp_username)
+        if (user and inp_username == user.username and inp_password == user.password):
+            self.user = user
+            print('Welcome,{username}!'.format(username = user.username))
+        else:
+            print('Password or Username is incorrect')
+        return self.user
+
+    def render_menu(self):
+        print("Menu Options:")
+        ControllerView.render_menu(self)
+
     def render_add_member(self):
         print("Enter member in field:")
-        resolver = DataResolver()
-        username = input("1# username>")
-        surname = input("2# surname>")
-        age = input("3# age>")
-        password = input("4# password>")
-
-        member_record = Member(
-            username,
-            surname,
-            age,
-            password
-        ),
+        username = input("1. username> ")
         # input("confirm? yes[y]/no[n]")
 
-        _user = Person.findbyname(username)
-        print(_user)
+        user = Person.findbyname(username)
+        if user:
+            print('Member: {name} already exists'.format(name = user.username))
+        else:
+            surname = input("2. surname> ")
+            age = input("3. age> ")
+            password = input("4. password> ")
+            new_member = Member(
+                username,
+                surname,
+                age,
+                password
+            )
+            new_member.add()
+            print("Member {username} was added succesfully".format(username = username))
+    def render_list_member(self):
+        print('list of active members')
+        print('- # - username - surname - age -')
+        list = Person.allmembers()
+        i = 0
+        for item in list:
+            user = item
+            i+= 1
+            print('- {i} - {name} - {surname} - {age} -'.format(i = i, name = user.username, surname = user.surname, age = user.age ))
+        return list
+    def render_edit_member(self):
+        list = self.render_list_member()
+        username = str(input("to edit, type username of member> "))
+        user = Person.findbyname(username, list)
+        if user:
+            surname = input("2. surname> ")
+            age = input("3. age> ")
+            password = input("4. password> ")
+            user = Member(
+                username,
+                surname,
+                age,
+                password
+            )
+            user.update()
+        else:
+            print('Member not found')
+
 
--- usermodels.py
+++ usermodels.py
@@ -1,26 +1,26 @@
 
 
-from ast import Pass
-from multiprocessing.spawn import prepare
 from datahelpers import DataResolver, JSONDataLayer, TargetFile
 
 class Person:
     def __init__(self, *args):
+        self.role = 'member'
         if len(args) > 2:
             self.username = args[0]
             self.surname = args[1]
             self.age = args[2]
             self.password = args[3]
-
+            if 4 in args:
+                self.role = args[4]
         else:
             self.username = args[0]['username']
             self.surname = args[0]['surname']
             self.age = args[0]['age']
             self.password = args[0]['password']
-        self.role = 'member'
+            self.role = args[0]['role']
 
     def toHeader(self):
-        return ["username", "surname", "age", "password", "role"]
+        return ["username", "surname", "age", "password", "role", "id"]
 
     def toRow(self):
         return {
@@ -38,9 +38,17 @@ class Person:
         return list
 
     @staticmethod
-    def findbyname(username):
-        user = False
+    def allmembers():
         list = Person.all()
+        check_admin = lambda o: o.role == 'admin'
+        filter(check_admin, list)
+        return list
+
+    @staticmethod
+    def findbyname(username, list = False):
+        user = False
+        if not list:
+            list = Person.all()
         if list:
             for item in list:
                 if item.username == username:
@@ -48,15 +56,30 @@ class Person:
         return user
     
     def add(self):
-        list_users = Person.all()
-        DataResolver.Save(resolver, list_users, TargetFile.Member)
+        resolver = DataResolver()
+        list = Person.all()
+        user = Person.findbyname(self.username, list)
+        if not user:
+            list.append(self)
+            DataResolver.Save(resolver, list, TargetFile.Member)
+            messagestr = 'member added'
+        return self
 
+    def update(self):
+        resolver = DataResolver()
+        list = Person.all()
+        user = Person.findbyname(self.username, list)
+        if not user:
+            list.append(self)
+            DataResolver.Save(resolver, list, TargetFile.Member)
+            messagestr = 'member added'
+        return self
     # def validate(self, username):
     #     user = self.findbyname(username)
     #     return user
 
 class Member(Person):
-    Pass
+    pass
     
 class LibraryAdmin(Person):
     def __init__ (self, *args):
