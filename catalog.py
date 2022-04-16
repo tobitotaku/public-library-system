087d10ca45e0ee5de8f83a49daddb12773e963af
tobitotaku <99360631+tobitotaku@users.noreply.github.com>
2022-04-16 12:55:30 +0200

    fix bookitem add, list, update, delete

--- catalog.py
+++ catalog.py
@@ -16,9 +16,9 @@ class Catalog:
 
     def __init__ (self):
         self.resolver = DataResolver()
-        self.allBooks = self.resolver.Read( TargetFile.Book, Book)
-        self.allItems = self.resolver.Read( TargetFile.LibraryItem, BookItem)
-
+        self.listAllBooks()
+        self.listAllBookItems()
+    
     def getBooks(self):
         # print(self.allBooks.)
         return self.allBooks
@@ -64,7 +64,7 @@ class Catalog:
             for i,item in enumerate(self.allBooks):
                 if item.id == id:
                     self.allBooks[i] = book
-                    self.resolver.Save(self.allBooks, TargetFile.Book)
+                    self.save()
                     return book
         return False
     
@@ -84,10 +84,19 @@ class Catalog:
                     self.resolver.Save(self.allItems, TargetFile.LibraryItem)
                     return id
 
+    def updateBookitem(self, id, bookitem):
+        for i,item in enumerate(self.allItems):
+            if item.id == id:
+                self.allItems[i] = bookitem
+                self.resolver.Save(self.allItems, TargetFile.LibraryItem)
+                return bookitem
+
     def listAllBooks(self):
+        self.allBooks = self.resolver.Read( TargetFile.Book, Book)
         return self.allBooks
 
     def listAllBookItems(self):
+        self.allItems = self.resolver.Read( TargetFile.LibraryItem, BookItem)
         return self.allItems
 
     def save(self):
--- controllers/bookitemcontroller.py
+++ controllers/bookitemcontroller.py
@@ -1,5 +1,5 @@
 from controllers.controllers import ControllerView
-from controllers.catalogcontroller import CatalogMemberCV
+from controllers.catalogcontroller import *
 
 class BookItemCV(ControllerView):
     def __init__(self, *args):
@@ -9,9 +9,11 @@ class BookItemCV(ControllerView):
             (self.render_add, "Add"),
             (self.render_edit, "Edit"),
             (self.render_delete, "Delete"),
-            (self.render_search, "Search"),
+            (self.render_main, "Back to main menu"),
             (exit, "Exit application"),
         ]
+        self.catalogcv = CatalogAdminCV(ControllerView)
+
     def render_main(self):
         if len(self.breadcrumbs)>0:
             self.breadcrumbs[0].render_menu()
@@ -25,27 +27,31 @@ class BookItemCV(ControllerView):
 
     def render_add(self):
         self.line()
-        self.render_list_catalog()        
-        id = input("[0] bookid? ")
-
+        self.catalogcv.render_list()        
+        try:
+            id = int(input("[0] bookid? "))
+        except:
+            print("Invalid option entered. Retry.")
+            self.render_add()
+        
         book = self.catalog.getBookById(id)
         if book:
-            self.catalog.addBookItem(id)
+            print(f'Book selected {book.id} {book.title}')
+            bookitem = self.catalog.addBookItem(id)
+            print(f'Bookitem {bookitem.id} added')
+            self.catalog.listAllBookItems()
         else:
             print('Book not found')
     
-    def render_list_catalog(self):
-        CatalogMemberCV(ControllerView).render_list()
-
     def render_list(self):
         self.line()
-        print('list of active members')
+        print('Bookitems in Library.')
         print('- ID - bookid - Author - Title - ISBN -')
         if len(self.catalog.allItems) == 0:
             print('Empty list.')
         for item in self.catalog.allItems:
-            book = self.catalog.getBookById(item.id)
-            print(f"- {item.id} - {item.bookid} - {item.author} - {item.title} - {item.ISBN} -")
+            book = self.catalog.getBookById(item.bookid)
+            print(f"- {item.id} - {item.bookid} - {book.author} - {book.title} - {book.ISBN} -")
 
     def render_edit(self):
         self.render_list()
@@ -55,12 +61,24 @@ class BookItemCV(ControllerView):
             print("Invalid option entered. Retry.")
             self.render_edit()
 
-        book = self.catalog.getBookItem(id)
-        if book:
-            confirm, book = self.edit_form(book)
-            if confirm:
-                self.catalog.update(id, book)
-                print(f"Bookitem {book.id} was changed succesfully")
+        bookitem = self.catalog.getBookItem(id)
+        if bookitem:
+            # confirm, book = self.edit_form(book)
+            book = self.catalog.getBookById(bookitem.bookid)
+            confirm_field = input(f"[{bookitem.id}] Book: [{book.title}] | edit field or skip? (y/Enter) ")
+            if confirm_field == 'y':
+                print('Books in Catalog')
+                self.catalogcv.render_list()
+                try:
+                    bookid = int(input(f'[0] Bookid? '))
+                except:
+                    print("Invalid ID entered. Retry.")
+                    self.render_edit()
+                confirm_save = input('Save changes or skip? (y/Enter) ')
+                if confirm_save == 'y':
+                    bookitem.bookid = bookid
+                    self.catalog.updateBookitem(id, bookitem)
+                    print(f"Bookitem {bookitem.id} was changed succesfully")
         else:
             print('Bookitem not found')
 
@@ -82,15 +100,4 @@ class BookItemCV(ControllerView):
                 print(f"Bookitem {bookitem.id} was NOT deleted")
         else:
             print('Bookitem not found')
-    
-    def render_search(self):
-        self.line()
-        query = str(input('Search by Title, Author, ISBN: '))
-        res = self.catalog.search(query)
-        if len(res) > 0:
-            print(res)
-        else:
-            print('No Books found')
 
-    def render_loan(self):
-        pass
