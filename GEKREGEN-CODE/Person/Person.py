aeaca0962898c4d06eae4e7083a3e8d887b27ef9
jessin <jessinrocker@gmail.com>
2022-06-13 02:47:53 +0200

    removed received code

--- GEKREGEN-CODE/Person/Person.py
+++
@@ -1,8 +0,0 @@
-class Person:
-    def __init__(self, gender, nameSet, firstName, surname, permissionLevel=1):
-        self.id = None
-        self.gender = gender
-        self.nameSet = nameSet
-        self.firstName = firstName
-        self.surname = surname
-        self.permissionLevel = permissionLevel
--- GEKREGEN-CODE/README.md
+++
@@ -1,1 +0,0 @@
-# Public Library System
